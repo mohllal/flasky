a8fd956fd8d73b457f6f0a9eb2f8e18e15651c58
Kareem Mohllal <kareem.mohllal@gmail.com>
2019-01-16 01:25:42 +0200

    Implement resource RESTful API endpoints

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api_v1 = Blueprint('api_v1', __name__)
 
-from . import authentication, errors
+from . import authentication, posts, users, comments, errors
---
+++ app/api_v1/decorators.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from flask import g
+from ..models import Permission
+from .errors import forbidden
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                return forbidden('Insufficient permissions')
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMIN)(f)
